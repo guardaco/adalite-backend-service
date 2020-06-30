120c4c64e6439c433d4253420becf0f3905cac62
Peter Benc <peter.benc1@gmail.com>
2020-07-01 01:50:42 +0200

    add tests for address summary endpoint

---
+++ test/integration/legacy/addressess-summary-integration-test.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai'
+import { runInServer } from '../test-utils'
+
+
+const ENDPOINT = '/addresses/summary/'
+
+describe('Address summary endpoint', () => {
+//   it('Should return something', async (done) =>
+//     runInServer(api =>
+//       api
+//         .get('/txs/summary/2ee3f67de64b52fc682c5d5e0ccfcfc81c056e3759b5739fa705b37576213540',
+//           (error, response) => {
+//             console.log(response, error)
+//           }).send(),
+//     ).then(done))
+
+//   it('Should return something', async (done) =>
+//     runInServer(api =>
+//       api
+//         .get('/addresses/summary/DdzFFzCqrhszp4fARmEMkgb99btzMKNuYnFMsQHXFZJyDPjPJuu3fi1JHCGgNgLSufYJaP6b7GMtTEprfmSzTQjKqJyneB75y5ABbiSf')
+//         .end((err, res) => {
+//           console.log(res, err)
+//           done()
+//         }),
+//     ))
+})
