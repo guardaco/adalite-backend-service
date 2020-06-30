63a55566393e09803d164bc852ba802345ee714a
Peter Benc <peter.benc1@gmail.com>
2020-07-01 01:51:12 +0200

    add tests for tx summary endpoint

---
+++ test/integration/legacy/txs-summary-integration-test.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai'
+import { runInServer } from '../test-utils'
+
+
+const ENDPOINT = '/txs/summary/'
+
+describe('Tx summary endpoint', () => {
+//   it('Should return something', async (done) =>
+//     runInServer(api =>
+//       api
+//         .get('/txs/summary/2ee3f67de64b52fc682c5d5e0ccfcfc81c056e3759b5739fa705b37576213540',
+//           (error, response) => {
+//             console.log(response, error)
+//           }).send(),
+//     ).then(done))
+
+//   it('Should return something', (done) =>
+//     runInServer(api =>
+//       api
+//         .get('/txs/summary/2ee3f67de64b52fc682c5d5e0ccfcfc81c056e3759b5739fa705b37576213540')
+//         .end((err, res) => {
+//           console.log(res, err)
+//           done()
+//         }),
+//     ))
+})
